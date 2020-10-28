efbaf91fa62913c442a65fc3f9d4ffe51cf805b7
Baistan <almazbekovbaistan@gmail.com>
2020-10-28 19:12:04 +0300

    headly functions of site

--- OnLineShop/store/models.py
+++ OnLineShop/store/models.py
@@ -1,23 +1,56 @@
 from django.db import models
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50, null=True)
+
+
 class Product(models.Model):
     CATEGORY = (
         ('Телефон','Телефон'),
         ('Компьютер','Компьютер'),
-        ('Компьютер','Компьютер'),
+        ('Видеокарта','Видеокарта'),
         ('Жесткий диск','Жесткий диск'),
     )
     name = models.CharField(max_length=50,null=True)
+    price = models.FloatField(null=True)
     category = models.CharField(max_length=50,null=True,choices=CATEGORY)
-    decsription = models.CharField(max_length=50,null=True)
+    decsription = models.CharField(max_length=50,null=True,blank=True)
     date_created = models.DateTimeField(auto_now_add=True,null=True)
+    tag = models.ManyToManyField(Tag,null=True)
 
 
     def __str__(self):
         return self.name
 
 
+class Customer(models.Model):
+    CATEGORY = (
+        ('Телефон','Телефон'),
+        ('Компьютер','Компьютер'),
+        ('Компьютер','Компьютер'),
+        ('Жесткий диск','Жесткий диск'),
+    )
+    full_name = models.CharField(max_length=200,null=True)
+    adress = models.CharField(max_length=200,null=True)
+    phone = models.CharField(max_length=200,null=True)
+    email = models.CharField(max_length=200,null=True)
+
+    def __str__(self):
+        return self.full_name
 
 
+class Order(models.Model):
+    STATUS = (
+        ('Pending','Pending'),
+        ('Delivered','Delivered'),
+        ('Not delivered','Not delivered'),
+    )
+    customer = models.ForeignKey(Customer,null=True,on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product,null=True,on_delete=models.SET_NULL)
+    date_created = models.DateTimeField(auto_now_add=True,null=True)
+    status = models.CharField(max_length=200,null=True,choices=STATUS)
+
+    def __str__(self):
+        return self.product.name
 
 
