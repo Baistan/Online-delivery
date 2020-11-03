84e5a179f0b0f8c16852aed3bd2262697b33ca10
baistan <almazbekovbaistan@gmail.com>
2020-11-03 19:29:25 +0600

    reworking path of pages

--- OnLineShop/store/urls.py
+++ OnLineShop/store/urls.py
@@ -4,12 +4,13 @@ from .views import *
 
 urlpatterns = [
     path('',home_page,name='home'),
-    path('register',register,name='register'),
-    path('login',login_page,name='login'),
-    path('logout',logout_page,name='logout'),
+    path('user/',user_page,name='user-page'),
+    path('register/',register,name='register'),
+    path('login/',login_page,name='login'),
+    path('logout/',logout_page,name='logout'),
     path('product/',products_page,name='products'),
     path('customer/<int:pk>/',customer_page,name='customer'),
     path('create_order/<int:pk>/',create_order,name='create_order'),
     path('update_order/<int:pk>/',update_order,name='update_order'),
-    path('delete_order/<int:pk>/',delete_order,name='delete_order')
+    path('delete_order/<int:pk>/',delete_order,name='delete_order'),
 ]
